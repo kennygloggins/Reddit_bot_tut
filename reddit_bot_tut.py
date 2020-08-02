63a027e7c87be3261e2ccdf0c8dc2c5906863d5d
kennygloggins <kenneth.t.smith@gmail.com>
2020-08-02 16:27:04 -0400

    -Print selected fields from top 5 post in hot

--- reddit_bot_tut.py
+++ reddit_bot_tut.py
@@ -3,4 +3,14 @@
 # File: reddit_bot_tut.py
 # Project: Reddit_bot
 
+import praw
 
+# Grab bot info from praw.ini
+reddit = praw.Reddit('bot1')
+subreddit = reddit.subreddit("modernwarfare")
+
+for submission in subreddit.hot(limit=5):
+    print("Title: ", submission.title)
+    print("Text: ", submission.selftext)
+    print("Score: ", submission.score)
+    print("---------------------------------\n")
